9475da148d693830da474a16dc6b95eb3250dbab
ashishtutorbin <ashish.tutorbin@gmail.com>
2021-08-14 03:13:45 +0530

    kjfhv

--- pwa/src/index.js
+++ pwa/src/index.js
@@ -1,8 +1,7 @@
 if("serviceWorker" in navigator){
     navigator.serviceWorker.register("sw.js").then(registeration=>{
-        console.log("sw Registered")
+      
     }).catch(error=>{
-        console.log("sw registration failed");
-        console.log(error)
+       
     })
 }
