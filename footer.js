dee805a54ca5ef1c40203f0c252e877d5a4d1ef8
ashishtutorbin <ashish.tutorbin@gmail.com>
2021-08-15 00:46:04 +0530

    kji

--- footer.js
+++ footer.js
@@ -1,14 +1,14 @@
 let canonical = window.location.href;
-// document.head.innerHTML+=`
-// <link rel="canonical" href="${canonical}"/>
+document.head.innerHTML+=`
+<link rel="canonical" href="${canonical}"/>
 
-// <meta name="robots" content="index, follow, all" />
-//   <meta http-equiv=”content-language” content=”en”>
-//   <meta name="geo.placename" content="United States" />
-//   <meta name="geo.region" content="usa" />
-//   <meta name="revisit-after" content="3 days">
+<meta name="robots" content="index, follow, all" />
+  <meta http-equiv=”content-language” content=”en”>
+  <meta name="geo.placename" content="United States" />
+  <meta name="geo.region" content="usa" />
+  <meta name="revisit-after" content="3 days">
   
-// `;
+`;
 
 const host='https://ashish221306.github.io';
 const currenturl=window.location.pathname;
